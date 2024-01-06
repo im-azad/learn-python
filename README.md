40fb2d1dd6f3eabd9baed156c2433d03ec589c44
im-azad <me.azad99@gmail.com>
2024-01-06 18:32:24 +0600

    Initial commit...

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# I am learning Python 
+
+## Python is powerful... and fast
+
+## Python runs everywhere;is friendly & easy to learn and it is Open.
---
+++ hello.py
@@ -0,0 +1,23 @@
+# print("hello world")
+
+temperature = 25
+if temperature > 30 : 
+    print("It's a hot day")
+elif temperature > 20 :
+    print("It's nice day")
+
+print("run code is done--")
+
+# x = 5
+# y = 10
+# print(x + y)
+
+x = "awesome"
+
+def myfunction():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunction()
+
+print("Python is " + x)
