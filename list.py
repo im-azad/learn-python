dc1a2616ae4e53499a7bd189273e8c87b8e485fc
im-azad <me.azad99@gmail.com>
2024-01-13 23:05:21 +0600

    extend an list or any iterable items

--- list.py
+++ list.py
@@ -3,4 +3,15 @@ fruitsList = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
 # add item at position
 fruitsList[1:3] = ["blackcurrant", "watermelon"]
 
+
+# insert at position 2
+fruitsList.insert(2, "watermelon") 
+
+# To add an item to the end of the list, use the append() method:
+fruitsList.append("Lucy")
+
+# extend another list 
+tropical = ["mango", "pineapple", "papaya"]
+fruitsList.extend(tropical)
+
 print(fruitsList)
