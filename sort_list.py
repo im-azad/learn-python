815eef71e2eb1b24ebf71e956632544c7d4c8295
im-azad <me.azad99@gmail.com>
2024-01-14 23:34:45 +0600

    customize sort function

--- sort_list.py
+++ sort_list.py
@@ -5,3 +5,14 @@ print(thislist)
 
 thislist.sort(reverse = True)
 print(thislist)
+
+
+# The function will return a number that will be used to sort the list (the lowest number first):
+def myfunc(n):
+  return abs(n - 50)
+
+numList = [100, 50, 65, 82, -10, 23]
+
+numList.sort(key = myfunc)
+
+print(numList)
